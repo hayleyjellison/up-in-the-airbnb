d246f5d12f27463187253f5826f6bb4367862096
c-l-nguyen <c.l.nguyen@icloud.com>
2020-01-19 11:59:50 -0600

    more pedantic website changes to remove extraneous spaces, fix formatting, comments, indentation

--- static/js/tableau.js
+++ static/js/tableau.js
@@ -1,20 +1,20 @@
 (function tabViz() {
-  
-    let bookingMapContainer = document.getElementById("bookingMap");
-    let bookingMapURL = "https://public.tableau.com/views/AirbnbAustinRegions/GroupBookingStory";
 
-    let airbnbTypesContainer = document.getElementById("airbnbTypes");
-    let airbnbTypesURL = "https://public.tableau.com/views/AirbnbType/AirbnbTypesStory";
+  let bookingMapContainer = document.getElementById("bookingMap");
+  let bookingMapURL = "https://public.tableau.com/views/AirbnbAustinRegions/GroupBookingStory";
 
-    let airbnbPropertyContainer = document.getElementById("airbnbProperty");
-    let airbnbPropertyURL = "https://public.tableau.com/views/AirBnB-metrics-by-region/AirBnBPropertyDetailsbyRegion";
+  let airbnbTypesContainer = document.getElementById("airbnbTypes");
+  let airbnbTypesURL = "https://public.tableau.com/views/AirbnbType/AirbnbTypesStory";
+
+  let airbnbPropertyContainer = document.getElementById("airbnbProperty");
+  let airbnbPropertyURL = "https://public.tableau.com/views/AirBnB-metrics-by-region/AirBnBPropertyDetailsbyRegion";
+
+  let options = {
+      // hideTabs: true
+  };
+
+  new tableau.Viz(bookingMapContainer, bookingMapURL, options);
+  new tableau.Viz(airbnbTypesContainer, airbnbTypesURL, options);
+  new tableau.Viz(airbnbPropertyContainer, airbnbPropertyURL, options);
   
-    let options = {
-        // hideTabs: true
-    };
-  
-    new tableau.Viz(bookingMapContainer, bookingMapURL, options);
-    new tableau.Viz(airbnbTypesContainer, airbnbTypesURL, options);
-    new tableau.Viz(airbnbPropertyContainer, airbnbPropertyURL, options);
-  
-  })()
+})()
